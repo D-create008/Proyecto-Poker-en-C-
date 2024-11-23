691de8a7ad8caac3edc2296d07a6a2a19c7bf2e2
D-create008 <a01712971@tec.mx>
2024-11-23 12:52:52 -0600

    Update Hand.h

--- Hand.h
+++ Hand.h
@@ -9,13 +9,13 @@ using namespace std;
 
 class Hand: public CardCollection{
   private:
-    int handSize;
+    int handSize;     //Hand size is an attribute, as well as cardList by inheritance
   public:
     Hand();
-    void replaceCard(int, Deck& deck);
-    void getHand(Deck& deck);
-    void showHand();
-    int evaluateHand();
-    void clearHand();
+    void replaceCard(int, Deck& deck);   //Method to replace a card in the Hand
+    void getHand(Deck& deck);       //Method to get the starting hand
+    void showHand();     //Method to print the current hand
+    int evaluateHand();    //Method to evaluate the strength of the hand
+    void clearHand();   //Method to empty the hand vector for the next round
 };
 #endif
