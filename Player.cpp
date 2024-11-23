d0f9d156b37f16c940acdfd7f12bf58f62a4809e
D-create008 <a01712971@tec.mx>
2024-11-23 12:53:39 -0600

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,22 +1,22 @@
 #include "Player.h"
 
-Player:: Player(){
-  name = "To be set";
+Player:: Player(){      //Constructor for player, declares the name, sets the balance to 100, and declares the current bet variable
+  name = "To be set"; 
   balance = 100;
   currentBet = 0;
 }
-void Player:: setName(string _name){
+void Player:: setName(string _name){  //Setter for name
   name = _name;
 }
-void Player:: setBalance(int _balance){
+void Player:: setBalance(int _balance){  //Setter for balance
   balance = _balance;
 }
-void Player:: makeBet(int _currentBet){
+void Player:: makeBet(int _currentBet){    //Setter for bet, but called makeBet
   currentBet = _currentBet;
 }
-int Player::getBet() {
+int Player::getBet() {           //Getter for bet, retrieves the current bet
     return currentBet;
 }
-int Player:: getBalance(){
+int Player:: getBalance(){       //Getter for balance, retrieves the current balance
   return balance;
 }
