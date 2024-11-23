14d39368410a6f2351595e19be509f1475b85312
D-create008 <a01712971@tec.mx>
2024-11-23 12:50:46 -0600

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -10,27 +10,31 @@ void Deck::createDeck()
 {
   string palo[4] = {"♥","♦","♣","♠"};
   string simbolo[13] = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+  //Makes the deck by using a for loop and adding each suit to each rank once
   for(int i=0; i<4; i++){
     for(int j=0; j<13; j++){
       addCard(simbolo[j]+palo[i]);
     }
   }  
+  //Current deck size when made
   deckSize = 52;
 }
-void Deck::shuffleDeck()
+void Deck::shuffleDeck() //Uses the shuffle vector algorithm and a random device to shuffle the elements in the deck vector
 {
   random_device rd; 
   default_random_engine gen(rd()); 
   shuffle(cardList.begin(), cardList.end(), gen); 
 }
 
-string Deck::dealCard(){
+string Deck::dealCard(){    //Deals a card and removes it from the deck
   string card = cardList.back();
   cardList.pop_back();
+  //Modify deck size to match its current value
   deckSize = deckSize - 1;
+  //Returns a card 
   return card;
 }
-void Deck:: clearDeck(){
+void Deck:: clearDeck(){  //Clears deck
   cardList.clear();
 }
 
