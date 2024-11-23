13e635d736ebc055e2f8080e63c9d7c6de8107b0
D-create008 <a01712971@tec.mx>
2024-11-23 12:50:04 -0600

    Update CardCollection.cpp

--- CardCollection.cpp
+++ CardCollection.cpp
@@ -1,10 +1,10 @@
 #include "CardCollection.h"
 
-CardCollection::CardCollection(vector<string> _cardList)
+CardCollection::CardCollection(vector<string> _cardList) //Constructor that initializes cardList
 {
   cardList= _cardList;
 }
-void CardCollection::addCard(string card)
+void CardCollection::addCard(string card)  //Ads a card to your card list
 {
   cardList.push_back(card);
 }
